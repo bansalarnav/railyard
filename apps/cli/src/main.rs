bbf736d50cb05bb30fdaaaab3d12663c80b5f070
Arnav Bansal <arnavbansal101@gmail.com>
2026-03-16 23:47:31 -0500

    batman

---
+++ apps/cli/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let config = config::AppConfig::default();
+    println!("Starting {} CLI", config.app_name);
+}
---
+++ apps/server/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let config = config::AppConfig::default();
+    println!("Starting {} server", config.app_name);
+}
